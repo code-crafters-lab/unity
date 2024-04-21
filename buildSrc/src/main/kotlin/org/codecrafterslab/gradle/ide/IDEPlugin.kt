ff63f2b9185828dd67e61f24a96fd020cacb1f44
Yujie Wu <coffee377@dingtalk.com>
2024-04-21 18:23:09 +0800

    :zap: ide 插件配置属性优化
    Signed-off-by: Yujie Wu <coffee377@dingtalk.com>

--- buildSrc/src/main/kotlin/org/codecrafterslab/gradle/ide/IDEPlugin.kt
+++ buildSrc/src/main/kotlin/org/codecrafterslab/gradle/ide/IDEPlugin.kt
@@ -1,23 +1,23 @@
 package org.codecrafterslab.gradle.ide
 
+import org.codecrafterslab.gradle.utils.PropertyUtils
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.ide.visualstudio.plugins.VisualStudioPlugin
 import org.gradle.ide.xcode.plugins.XcodePlugin
 import org.gradle.plugins.ide.eclipse.EclipsePlugin
 import org.gradle.plugins.ide.idea.IdeaPlugin
-import java.io.File
 
 /**
  * 集成开发环境插件配置
  */
 class IDEPlugin : Plugin<Project> {
     companion object {
-        const val IDE_PROPERTY_NAME: String = "ide"
+        const val IDE_PROPERTY_NAME: String = "dev.ide"
     }
 
     override fun apply(project: Project) {
-        val ide = IDE.of(project.rootProject.properties.getOrDefault(IDE_PROPERTY_NAME, "").toString())
+        val ide = IDE.of(PropertyUtils.getPriorityProperty(IDE_PROPERTY_NAME, project, ""))
         when (ide) {
             IDE.ECLIPSE -> project.plugins.apply(EclipsePlugin::class.java)
             IDE.VISUAL_STUDIO -> project.plugins.apply(VisualStudioPlugin::class.java)
@@ -25,10 +25,5 @@ class IDEPlugin : Plugin<Project> {
             IDE.IDEA -> project.plugins.apply(IdeaPlugin::class.java)
             IDE.NONE -> {}
         }
-    }
-
-    private fun isRootProject(project: Project): Boolean {
-        return project.projectDir.relativeTo(project.rootProject.projectDir).path.split(File.separator)
-            .joinToString("/") == ""
     }
 }
