dd16121b565b07daef8d05f43d30641590deaf1f
Yujie Wu <coffee377@dingtalk.com>
2025-05-26 21:43:32 +0800

    ⚡️ 添加GCExcel测试代码
    Signed-off-by: Yujie Wu <coffee377@dingtalk.com>

--- dict/dict-starter/build.gradle.kts
+++ dict/dict-starter/build.gradle.kts
@@ -5,4 +5,6 @@ dependencies {
     api(project(":dict-api"))
     api(project(":dict-core"))
     api(project(":dict-autoconfigure"))
+
+    testImplementation("com.grapecitysoft.documents:gcexcel:8.0.6")
 }
